f0e5df1ff75f5e90d6ef9019db79526e53f9d513
饶家兴 <1256128692@qq.com>
2019-12-10 19:40:27 +0800

    XXX已经提交

---
+++ src/main/java/com/offcn/controller/ReceiveMsgController.java
@@ -0,0 +1,24 @@
+package com.offcn.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReceiveMsgController {
+
+    @Autowired
+    private JmsMessagingTemplate jmsMessagingTemplate;
+
+    @JmsListener(destination = "pkq")
+
+    @RequestMapping("/getSend")
+    public void getMsg(String msg){
+
+        System.out.println("接收到的消息是"+msg);
+
+    }
+
+}
